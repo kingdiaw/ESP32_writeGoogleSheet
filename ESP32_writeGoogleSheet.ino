782e02cc41abb60de0c4f43ebe88b91125a50865
kingdiaw <kingdiawehsut@gmail.com>
2022-06-25 15:01:47 +0800

    Update ESP32_writeGoogleSheet.ino

--- ESP32_writeGoogleSheet.ino
+++ ESP32_writeGoogleSheet.ino
@@ -1,9 +1,27 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+WiFiManager wm;
 
+void setup() {
+    WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP    
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+    
+    //reset settings - wipe credentials for testing
+    //wm.resetSettings();
+
+    wm.setConfigPortalBlocking(false);
+    wm.setConfigPortalTimeout(60);
+    //automatically connect using saved credentials if they exist
+    //If connection fails it starts an access point with the specified name
+    if(wm.autoConnect("AutoConnectAP")){
+        Serial.println("connected...yeey :)");
+    }
+    else {
+        Serial.println("Configportal running");
+    }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+    wm.process();
+    // put your main code here, to run repeatedly:
 }
